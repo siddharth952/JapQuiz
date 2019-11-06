19b534680e9b2e3c698194d4a4c1f86c7433d9b1
Siddharth <40488679+siddharth952@users.noreply.github.com>
2019-11-06 23:04:09 +0530

    Added onClickListenerEvent for "BackToHome" Button

--- app/src/main/java/com/halfpolygon/quizzer/SecondScreen.java
+++ app/src/main/java/com/halfpolygon/quizzer/SecondScreen.java
@@ -2,8 +2,10 @@ package com.halfpolygon.quizzer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -33,5 +35,11 @@ public class SecondScreen extends AppCompatActivity {
         textBtn.setTypeface(typeface);
 
 
+    }
+
+
+    public void backBtnTapped(View view){
+        Intent b = new Intent(SecondScreen.this,MainActivity.class);
+        startActivity(b);
     }
 }
